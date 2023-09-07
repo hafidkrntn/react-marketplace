31bcf4fa59925fdbfc455e76d90a9a281b57831e
hafidkrntn <hafidkrntn@gmail.com>
2023-09-07 13:17:18 +0700

    first commit

---
+++ src/components/socials/Socials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className='socials-row'> 
+        <h1>Follow us on Social Networks</h1>
+        <div className="socials-group">
+            <a href="https://www.facebook.com/strykinnfluous/"><img src="./assets/images/others/fb.png" alt="" /></a>
+            <a href="/https://www.instagram.com/hakunnnn/?hl=id"><img src="./assets/images/others/in.png" alt="" /></a>
+            <a href="/https://www.linkedin.com/in/hafid-kurnianton-123958219/"><img src="./assets/images/others/li.png" alt="" /></a>
+        </div>
+    </div>
+  )
+}
+
+export default Socials
